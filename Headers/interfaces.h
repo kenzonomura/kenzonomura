7a8c7e747c0f2edf1e36a8c295432897d8de012f
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-12-03 11:44:36 -0300

    Update interfaces.h

--- Headers/interfaces.h
+++ Headers/interfaces.h
@@ -1,33 +1,98 @@
-#ifndef INTERFACES_H_INCLUDED
-#define INTERFACES_H_INCLUDED
+#ifndef INTERFACES_HPP_INCLUDED
+#define INTERFACES_HPP_INCLUDED
 
-// Incluir cabeçalhos.
+#include <string>
+#include <vector>
+#include "entidades.h"
+#include <optional>
 
-#include "dominios.h"
-#include <stdexcept>
-
-using namespace std;
-
-// Declarações adiantadas.
-
-class ILNAutenticacao;
-class ILNGerente;
-
-// Declaração de interface da camada de apresentação.
-
-class IUAutenticacao {
+class IControleLogin {
 public:
-    virtual bool autenticar() = 0;                                  // Método por meio do qual é solicitado serviço.
-    virtual void setCntrLNAutenticacao(ILNAutenticacao *) = 0;      // Método para ligar (link) controladora na camada de serviço.
-    virtual ~IUAutenticacao(){}                                     // Método destrutor virtual.
+    virtual bool autenticar(const std::string& email, const std::string& senha) = 0;
+    virtual ~IControleLogin() {}
 };
 
-// Declaração de interface da camada de serviço.
-
-class ILNAutenticacao {
+class IControleCadastro {
 public:
-    virtual bool autenticar(const Matricula&, const Senha&) = 0;    // Método por meio do qual é solicitado serviço.
-    virtual ~ILNAutenticacao(){}                                    // Método destrutor virtual.
+    virtual bool cadastrarUsuario(const std::string& emailStr, const std::string& nomeStr, const std::string& senhaStr) = 0;
+    virtual ~IControleCadastro() {}
+    
 };
 
-#endif // INTERFACES_H_INCLUDED
+class IControleQuadros {
+public:
+    virtual std::vector<Quadro> obterQuadros(const std::string& emailUsuario) = 0;
+    virtual bool criarQuadro(const std::string& emailUsuario, 
+                     const std::string& codigo, 
+                     const std::string& nome, 
+                     const std::string& descricao, 
+                     int limite) = 0;
+    virtual bool editarQuadro(const std::string& emailUsuario, 
+                      const std::string& codigoQuadro, 
+                      const std::optional<std::string>& novoNome, 
+                      const std::optional<std::string>& novaDescricao, 
+                      const std::optional<int>& novoLimite) = 0;
+    virtual bool excluirQuadro(const std::string& emailUsuario, const std::string& codigoQuadro) = 0;
+    virtual ~IControleQuadros() {}
+    
+};
+
+// ---------------------------------------------------- CAMADA DE SERVICOS --------------------------------------------------- //
+
+// Interface para gerenciamento de usuÃ¡rios
+
+class IGerenciamentoUsuario {
+public:
+    virtual void criarUsuario(const Conta& conta) = 0;
+    virtual void editarUsuario(const std::string& email, const std::string& novoNome, const std::string& novaSenha) = 0;
+    virtual void excluirUsuario(const std::string& email) = 0;
+    virtual ~IGerenciamentoUsuario() {}
+};
+
+// Interface para gerenciamento de quadros (boards)
+class IGerenciamentoQuadro {
+public:
+    virtual void criarQuadro(const std::string& emailConta, const Quadro& quadro) = 0;
+    virtual std::optional<Quadro> visualizarQuadro(const std::string& emailUsuario, const std::string& codigo) = 0;
+    virtual void editarQuadro(const std::string& emailUsuario, const std::string& codigo, const std::optional<std::string>& novoNome, const std::optional<std::string>& novaDescricao, const std::optional<int>& novoLimite) = 0;
+    virtual void excluirQuadro(const std::string& emailUsuario, const std::string& codigo) = 0;
+    virtual ~IGerenciamentoQuadro() {}
+};
+
+// Interface para gerenciamento de cartÃµes (cards)
+class IGerenciamentoCartao {
+public:
+    virtual void criarCartao(const Cartao& cartao, const std::string& codigoQuadro, const std::string& emailUsuario) = 0;
+    virtual std::optional<Cartao> visualizarCartao(const std::string& codigoCartao, const std::string& emailUsuario) = 0;
+    virtual void moverCartao(const std::string& codigoCartao, const std::string& novaColuna, const std::string& emailUsuario) = 0;
+    virtual void excluirCartao(const std::string& codigoCartao, const std::string& emailUsuario) = 0;
+    virtual ~IGerenciamentoCartao() {}
+};
+
+// ---------------------------------------------------- CAMADA DE APRESENTACAO --------------------------------------------------- //
+
+class ITelaInicial {
+public:
+    virtual void exibir() = 0;
+    virtual ~ITelaInicial() {}
+};
+
+class ITelaCadastro {
+public:
+    virtual void exibir() = 0;
+    virtual ~ITelaCadastro() {}
+};
+
+class ITelaLogin {
+public:
+    virtual void exibir() = 0;
+    virtual ~ITelaLogin() {}
+};
+
+class ITelaQuadros {
+public:
+    virtual void exibir() = 0;
+    virtual ~ITelaQuadros() {}
+};
+
+#endif // INTERFACES_HPP_INCLUDED
