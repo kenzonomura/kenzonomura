2d716963655898231a09163dc0b2b7b1a8f552aa
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-12-03 11:42:03 -0300

    Update controladorasservico.h

--- Headers/controladorasservico.h
+++ Headers/controladorasservico.h
@@ -1,27 +1,48 @@
-#ifndef CONTROLADORASSERVICO_H_INCLUDED
-#define CONTROLADORASSERVICO_H_INCLUDED
+#ifndef SERVICOS_HPP_INCLUDED
+#define SERVICOS_HPP_INCLUDED
 
-#include "dominios.h"
+#include "interfaces.hpp"
+#include <optional>
+#include <string>
 #include "entidades.h"
-#include "interfaces.h"
+#include "DatabaseManager.hpp"
 
-
-//--------------------------------------------------------------------------------------------
-// Declarações de classes controladoras da camada de serviços.
-//
-// Falta implementar códigos.
-
-class CntrServicoAutenticacao:public IServicoAutenticacao{
+class ServicosConta : public IGerenciamentoUsuario {
+private:
+    DatabaseManager dbManager;  // Declarando o campo dbManager
+public:
+    ServicosConta(const std::string& dbPath);
+    void criarUsuario(const Conta& conta) override; // CorreÃ§Ã£o aqui
+    void editarUsuario(const std::string& email, const std::string& novoNome, const std::string& novaSenha) override;
+    void excluirUsuario(const std::string& email) override;
+    std::optional<Conta> visualizarUsuario(const std::string& email);
 };
 
-//--------------------------------------------------------------------------------------------
 
-class CntrServicoPessoal:public IServicoPessoal{
+class ServicosQuadro : public IGerenciamentoQuadro{
+public:
+    
+    ServicosQuadro(const std::string& dbPath);
+    void criarQuadro(const std::string& emailConta, const Quadro& quadro);
+    void editarQuadro(const std::string& emailUsuario, const std::string& codigo, const std::optional<std::string>& novoNome, const std::optional<std::string>& novaDescricao, const std::optional<int>& novoLimite);
+    void excluirQuadro(const std::string& emailUsuario, const std::string& codigo);
+    std::optional<Quadro> visualizarQuadro(const std::string& emailUsuario, const std::string& codigo);
+
+private:
+    DatabaseManager dbManager;
 };
 
-//--------------------------------------------------------------------------------------------
 
-class CntrServicoProdutosFinanceiros:public IServicoProdutosFinanceiros{
+class ServicosCartao : public IGerenciamentoCartao {
+private:
+    DatabaseManager dbManager;
+
+public:
+    ServicosCartao(const std::string& dbPath);
+    void criarCartao(const Cartao& cartao, const std::string& codigoQuadro, const std::string& emailUsuario) override;
+    std::optional<Cartao> visualizarCartao(const std::string& codigoCartao, const std::string& emailUsuario) override;
+    void moverCartao(const std::string& codigoCartao, const std::string& novaColuna, const std::string& emailUsuario) override;
+    void excluirCartao(const std::string& codigoCartao, const std::string& emailUsuario) override;
 };
 
-#endif // CONTROLADORASSERVICO_H_INCLUDED
+#endif // SERVICOS_HPP_INCLUDED
