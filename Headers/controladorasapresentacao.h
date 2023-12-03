28d0b79530262b9067155e272bf10a2ac15ec89c
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-12-03 11:40:51 -0300

    Update controladorasapresentacao.h

--- Headers/controladorasapresentacao.h
+++ Headers/controladorasapresentacao.h
@@ -1,97 +1,39 @@
-#ifndef CONTROLADORASAPRESENTACAO_H_INCLUDED
-#define CONTROLADORASAPRESENTACAO_H_INCLUDED
+#ifndef APRESENTACAO_H
+#define APRESENTACAO_H
 
-#include <string.h>
-#include "curses.h"
-#include "dominios.h"
-#include "entidades.h"
-#include "interfaces.h"
+#include "interfaces.hpp"
+#include <iostream>
+#include "controle.hpp"
 
-
-//--------------------------------------------------------------------------------------------
-// Declarações de classes controladoras e implementações de métodos.
-
-class CntrApresentacaoControle{
-    private:
-        CPF cpf;
-        IApresentacaoAutenticacao *cntrApresentacaoAutenticacao;
-        IApresentacaoPessoal *cntrApresentacaoPessoal;
-        IApresentacaoProdutosFinanceiros *cntrApresentacaoProdutosFinanceiros;
-    public:
-        void executar();
-        void setCntrApresentacaoAutenticacao(IApresentacaoAutenticacao*);
-        void setCntrApresentacaoPessoal(IApresentacaoPessoal*);
-        void setCntrApresentacaoProdutosFinanceiros(IApresentacaoProdutosFinanceiros*);
+class TelaInicial : public ITelaInicial {
+public:
+    TelaInicial();
+    void exibir() override;
 };
 
-inline void CntrApresentacaoControle::setCntrApresentacaoAutenticacao(IApresentacaoAutenticacao *cntr){
-    cntrApresentacaoAutenticacao = cntr;
-}
-
-inline void CntrApresentacaoControle::setCntrApresentacaoPessoal(IApresentacaoPessoal *cntr){
-            cntrApresentacaoPessoal = cntr;
-}
-
-inline void CntrApresentacaoControle::setCntrApresentacaoProdutosFinanceiros(IApresentacaoProdutosFinanceiros *cntr){
-    cntrApresentacaoProdutosFinanceiros = cntr;
-}
-
-//--------------------------------------------------------------------------------------------
-
-class CntrApresentacaoAutenticacao:public IApresentacaoAutenticacao{
-    private:
-        IServicoAutenticacao *cntr;
-    public:
-        bool autenticar(CPF*);
-        void setCntrServicoAutenticacao(IServicoAutenticacao*);
+class TelaCadastro : public ITelaCadastro {
+public:
+    TelaCadastro();
+    void exibir() override;
 };
 
-inline void CntrApresentacaoAutenticacao::setCntrServicoAutenticacao(IServicoAutenticacao *cntr){
-    this->cntr = cntr;
-}
+class TelaLogin : public ITelaLogin {
+private:
+    ControladorLogin controleLogin;
 
-//--------------------------------------------------------------------------------------------
-
-class CntrApresentacaoPessoal:public IApresentacaoPessoal{
-    private:
-        IServicoPessoal *cntrServicoPessoal;
-        IServicoProdutosFinanceiros *cntrServicoProdutosFinanceiros;
-        void consultarDadosPessoais();
-    public:
-        void executar(CPF);
-        void cadastrar();
-        void setCntrServicoPessoal(IServicoPessoal*);
-        void setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros*);
+public:
+    TelaLogin(const std::string& dbPath);
+    void exibir() override;
 };
 
-inline void CntrApresentacaoPessoal::setCntrServicoPessoal(IServicoPessoal *cntr){
-    cntrServicoPessoal = cntr;
-}
+class TelaGerenciamentoQuadros : public ITelaQuadros {
+private:
+    ControladorQuadros controladorQuadros;
+    std::string emailUsuario;
 
-inline void CntrApresentacaoPessoal::setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros *cntr){
-    cntrServicoProdutosFinanceiros = cntr;
-}
-
-//--------------------------------------------------------------------------------------------
-
-class CntrApresentacaoProdutosFinanceiros:public IApresentacaoProdutosFinanceiros{
-    private:
-        IServicoProdutosFinanceiros *cntr;
-        void consultarConta();
-        void cadastrarProdutoInvestimento();
-        void descadastrarProdutoInvestimento();
-        void consultarProdutoInvestimento();
-        void realizarAplicacao();
-        void listarAplicacoes();
-    public:
-        void executar();
-        void executar(CPF);
-        void setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros*);
+public:
+    TelaGerenciamentoQuadros(const std::string& dbPath, const std::string& emailUsuario);
+    void exibir() override;
 };
 
-inline void CntrApresentacaoProdutosFinanceiros::setCntrServicoProdutosFinanceiros(IServicoProdutosFinanceiros *cntr){
-    this->cntr = cntr;
-}
-
-
-#endif // CONTROLADORASAPRESENTACAO_H_INCLUDED
+#endif // APRESENTACAO_H
